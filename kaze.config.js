5b480b0c0a6eda98eee54bfc21a6ad83566a34aa
Kazetama <tamaketuahimpunan@gmail.com>
2026-02-23 22:09:58 +0700

    chore: clean up old project files

--- kaze.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    appName: "Kaze",
-    port: 3000
-};
--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import config from "./kaze.config.js";
-
-const app = express();
-app.use(express.json());
-
-app.listen(config.port, () => {
-    console.log(`Kaze running on port ${config.port}`);
-});
